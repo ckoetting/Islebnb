efc32a387f6db2d28ed8cd914ae4af69b60f6154
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-08-25 15:34:44 +0200

    Create listings show page, added some migrations for more precise listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -40,7 +40,7 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :description, photos: [])
+    params.require(:listing).permit(:title, :description, :price, photos: [])
   end
 
 end

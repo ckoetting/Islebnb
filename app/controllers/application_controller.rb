5d36da5eae673aa34a806e9869f05fe0d2d762d8
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-08-27 12:18:12 +0200

    user avatar now displays correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected 
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :photo, :description])
   end
 end

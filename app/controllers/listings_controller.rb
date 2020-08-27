5d8166faf3cbbdd418204fab48dd822550353c37
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-08-27 12:44:37 +0200

    fixed a bug with the index page not showing anything if nothing was being searched

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,10 @@
 class ListingsController < ApplicationController
   def index
+    if params[:search_by_title_and_price].nil? || params[:search_by_title_and_price].empty?
+      @listings = Listing.all
+    else
       @listings = Listing.search_by_title_and_price(params[:search_by_title_and_price])
+    end
   end
   
 

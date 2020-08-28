a3e23bb54e5397c7d20d0f137706c0f00062101e
Filippo Lanza <fpg.lanza@gmail.com>
2020-08-28 14:45:25 +0200

    bugs fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  def show 
+  def show
     @user = User.find(params[:id])
   end
-  
+
   def my_booking_requests
     @my_booking_requests = []
     current_user.listings.each do |listing|
@@ -12,7 +12,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def my_bookings 
+  def my_listings
+    @my_listings = current_user.listings
+  end
+
+  def my_bookings
     @my_bookings = current_user.bookings
   end
 end

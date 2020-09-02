b751cc0328e1293e34c19b7e76892f076b9b6046
HC Tung <hc.tung@gmail.com>
2020-09-02 14:40:49 +0200

    added autocomplete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_102225) do
+ActiveRecord::Schema.define(version: 2020_08_27_094739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,9 +77,7 @@ ActiveRecord::Schema.define(version: 2020_08_28_102225) do
     t.bigint "listing_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["listing_id"], name: "index_reviews_on_listing_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -101,5 +99,4 @@ ActiveRecord::Schema.define(version: 2020_08_28_102225) do
   add_foreign_key "bookings", "users"
   add_foreign_key "listings", "users"
   add_foreign_key "reviews", "listings"
-  add_foreign_key "reviews", "users"
 end
